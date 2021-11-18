1270771a62290c95cde3bbcb6d10a91efb52c1b4
sunchi <rachanlamoolphal@gmail.com>
2021-11-18 14:51:57 +0700

    show image from imshow

--- readimage.py
+++ readimage.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 
-img = cv.imread('square.png',1)
-print(img.ndim)
+img = cv.imread('square.png',0)
+print(img.ndim)#show image
+cv.imshow('show image gray color',img)
 cv.waitKey(0)
 cv.destroyAllWindows()
