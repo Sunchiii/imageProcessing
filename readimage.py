a6d1d2b29c96d7aa0d5e78d0d245daaae09f2554
sunchi <rachanlamoolphal@gmail.com>
2021-11-18 14:58:42 +0700

    resize image

--- readimage.py
+++ readimage.py
@@ -2,6 +2,10 @@ import cv2 as cv
 
 img = cv.imread('square.png',0)
 print(img.ndim)#show image
-cv.imshow('show image gray color',img)
+width = int(img.shape[0]*80/100)
+hieght = int(img.shape[1]*80/100)
+dim = (width,hieght)
+resize = cv.resize(img,dim)
+cv.imshow('show image gray color',resize)
 cv.waitKey(delay=10000)#show image 10sec if is 0 maen show without time
 cv.destroyAllWindows()
