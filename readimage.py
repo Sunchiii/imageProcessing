cc1862ef2f663c4b094840bf982756145cd880c8
sunchi <rachanlamoolphal@gmail.com>
2021-11-18 15:08:17 +0700

    write image to this directory

--- readimage.py
+++ readimage.py
@@ -6,6 +6,7 @@ width = int(img.shape[0]*80/100)
 hieght = int(img.shape[1]*80/100)
 dim = (width,hieght)
 resize = cv.resize(img,dim)
+cv.imwrite('gray.jpg',resize)
 cv.imshow('show image gray color',resize)
 cv.waitKey(delay=10000)#show image 10sec if is 0 maen show without time
 cv.destroyAllWindows()
