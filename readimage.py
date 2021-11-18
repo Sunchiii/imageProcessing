2ecab370d23f9522d51bbc1b46c29f9ae08dcc90
sunchi <rachanlamoolphal@gmail.com>
2021-11-18 14:54:05 +0700

    add delay to show image

--- readimage.py
+++ readimage.py
@@ -3,5 +3,5 @@ import cv2 as cv
 img = cv.imread('square.png',0)
 print(img.ndim)#show image
 cv.imshow('show image gray color',img)
-cv.waitKey(0)
+cv.waitKey(delay=10000)#show image 10sec if is 0 maen show without time
 cv.destroyAllWindows()
