c13f68ec91d74a4d3693f32350be40023404d0b7
sunchi <rachanlamoolphal@gmail.com>
2021-11-20 23:22:35 +0700

    Video recording

--- videoCap.py
+++ videoCap.py
@@ -1,16 +1,20 @@
 import cv2 as cv
 
-cap = cv.VideoCapture('image/Video.mp4')
+cap = cv.VideoCapture(0)
+four_cc = cv.VideoWriter_fourcc(*'XVID')#type of record video
+result = cv.VideoWriter('grayVideo.avi',four_cc,20.0,(600,600))
 
 while cap.isOpened():
     ref,frame = cap.read()#ref is boolean status read image#frame is image
+
     if ref is True:
-        grayImage = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
-        cv.imshow('openVideo',grayImage)
+        cv.imshow('openVideo',frame)
+        result.write(frame)
     else:
         break
     if cv.waitKey(1) and 0xFF == ord("e"):
         break
+result.release()
 cap.release()
 cv.destroyAllWindows()
     
