581e2bcc165f86301442965169b812c6d4fb4e14
sunchi <rachanlamoolphal@gmail.com>
2021-11-21 18:01:29 +0700

    drawing cirle

--- drawing.py
+++ drawing.py
@@ -3,9 +3,9 @@ import cv2 as cv
 img = cv.imread('image/cat.jpg')
 resize = cv.resize(img,(600,600))
 
-#draw rectangle
-#rectangle(image , (top,left),(bottom,right),(BGR),5)
-cv.rectangle(resize,(50,50),(200,200),(255,0,0),5)
+#draw circle
+#cv.circle(image,(center point x,y),scale,(color BGR),border)
+cv.circle(resize,(426,349),100,(0,255,0),5)
 cv.imshow('output',resize)
 cv.waitKey(0)
 cv.destroyAllWindows()
