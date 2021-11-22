9f2633caf82256ebd7a03c8c1589b5beffd2ceaf
sunchi <rachanlamoolphal@gmail.com>
2021-11-22 18:37:05 +0700

    create connect line from point before to point after

--- drawing.py
+++ drawing.py
@@ -3,9 +3,16 @@ import cv2 as cv
 img = cv.imread('image/cat.jpg')
 resize = cv.resize(img,(600,600))
 
-#draw text
-#cv.putText(image,'text',(position x,y),font#hersheyfontOnOpemcv,font scale,(color BGR),bold)
-cv.putText(resize,'this is a cat',(10,50),1,4,(0,0,0))
+point = []
+def ConnectPoint(event,x,y,flags,params):
+    if event is cv.EVENT_LBUTTONDOWN:
+        cv.circle(resize,(x,y),10,(0,0,255),4)
+        point.append((x,y))
+        if len(point) >= 2 :
+             cv.line(resize,point[-2],point[-1],(0,255,0),5)
+        cv.imshow('output',resize)
+        
 cv.imshow('output',resize)
+cv.setMouseCallback('output',ConnectPoint)
 cv.waitKey(0)
 cv.destroyAllWindows()
