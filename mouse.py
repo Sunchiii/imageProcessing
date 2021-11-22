c7a7b93281e667fe95827c31bdd528fd806cbfb0
sunchi <rachanlamoolphal@gmail.com>
2021-11-22 17:05:01 +0700

    read color pixel position from image show to this image

--- mouse.py
+++ mouse.py
@@ -4,7 +4,10 @@ img = cv.imread('image/tree.jpg')
 
 def showposition(event,x,y,flags,params):
     if event == cv.EVENT_LBUTTONDOWN:
-       text = str(x)+'.'+str(y)
+       blue = img[y,x,0]#[position x,position y,index color 0|1|3]
+       green = img[y,x,1]
+       red = img[y,x,2]
+       text = str(blue)+'.'+str(green)+'.'+str(red)
        cv.putText(img,text,(x,y),cv.FONT_HERSHEY_SIMPLEX,1,(255,0,0),cv.LINE_4) 
        cv.imshow('output',img)
 
