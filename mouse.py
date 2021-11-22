049c2ccbc5230b64a94bbdd67138c5858bdda577
sunchi <rachanlamoolphal@gmail.com>
2021-11-22 16:53:06 +0700

    write text on position from mouse to image

---
+++ mouse.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+
+img = cv.imread('image/tree.jpg')
+
+def showposition(event,x,y,flags,params):
+    if event == cv.EVENT_LBUTTONDOWN:
+       cv.putText(img,"yes",(x,y),cv.FONT_HERSHEY_SIMPLEX,1,(255,0,0),cv.LINE_4) 
+       cv.imshow('output',img)
+
+cv.imshow('output',img)
+cv.setMouseCallback('output',showposition)#(discription,funtion)
+cv.waitKey(0)
+cv.destroyAllWindows()
