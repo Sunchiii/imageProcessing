053bf07e846f724f27698ee7c5dc4ea148232415
sunchi <rachanlamoolphal@gmail.com>
2021-11-22 18:08:42 +0700

    read color pixel from position then show color to new windows

--- mouse.py
+++ mouse.py
@@ -1,17 +1,21 @@
 import cv2 as cv
-
+import numpy as np
 img = cv.imread('image/tree.jpg')
 
-def showposition(event,x,y,flags,params):
+def readposition(event,x,y,flags,params):
     if event == cv.EVENT_LBUTTONDOWN:
-       blue = img[y,x,0]#[position x,position y,index color 0|1|3]
-       green = img[y,x,1]
-       red = img[y,x,2]
-       text = str(blue)+'.'+str(green)+'.'+str(red)
-       cv.putText(img,text,(x,y),cv.FONT_HERSHEY_SIMPLEX,1,(255,0,0),cv.LINE_4) 
-       cv.imshow('output',img)
+        blue = img[y,x,0]
+        green = img[y,x,1]
+        red = img[y,x,2]
+
+    
+        imgcolor = np.zeros([300,300,3],np.uint8)#[width,hieght,color index],numpy.uint8
+        imgcolor[:] = [blue,green,red]
+        cv.imshow('result',imgcolor)
+
+
 
 cv.imshow('output',img)
-cv.setMouseCallback('output',showposition)#(discription,funtion)
+cv.setMouseCallback('output',readposition)
 cv.waitKey(0)
 cv.destroyAllWindows()
