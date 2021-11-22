58a60d8766dd558aae616b0e4bbf12b8d7a6908b
sunchi <rachanlamoolphal@gmail.com>
2021-11-22 16:57:15 +0700

    write position show to image

--- mouse.py
+++ mouse.py
@@ -4,7 +4,8 @@ img = cv.imread('image/tree.jpg')
 
 def showposition(event,x,y,flags,params):
     if event == cv.EVENT_LBUTTONDOWN:
-       cv.putText(img,"yes",(x,y),cv.FONT_HERSHEY_SIMPLEX,1,(255,0,0),cv.LINE_4) 
+       text = str(x)+'.'+str(y)
+       cv.putText(img,text,(x,y),cv.FONT_HERSHEY_SIMPLEX,1,(255,0,0),cv.LINE_4) 
        cv.imshow('output',img)
 
 cv.imshow('output',img)
